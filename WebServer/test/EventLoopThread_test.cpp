5c1e4f2a88f3283bcab7d180b88d66b4cb78a411
yeweili94 <yeweili94@gmail.com>
2019-01-03 16:50:18 +0800

    add test

---
+++ WebServer/test/EventLoopThread_test.cpp
@@ -0,0 +1,28 @@
+#include <WebServer/EventLoopThread.h>
+#include <WebServer/EventLoop.h>
+
+#include <stdio.h>
+#include <boost/bind.hpp>
+
+using namespace ywl;
+using namespace ywl::net;
+
+void funInthread()
+{
+    printf("runInThread():pid = %d;tid = %d\n", getpid(), CurrentThread::tid());
+}
+
+int main()
+{
+    printf("main() : pid = %d; tid = %d\n", getpid(), CurrentThread::tid());
+    funInthread();
+    EventLoopThread loopThread;
+    EventLoop* loop = loopThread.startLoop();
+    //异步调用runInLoop
+    // loop->runInLoop(boost::bind(funInthread));
+    sleep(2);
+    //异步调用
+    // loop->runAfter(2, boost::bind(funInthread));
+    loop->quit();
+    printf("main() exit\n");
+}
