fc48e22e9052261a25577097e6e8364562d5767f
yeweili94 <yeweili94@gmail.com>
2019-03-09 10:22:57 +0800

    add memory barrier to support double check

--- WebServer/base/Singleton.h
+++ WebServer/base/Singleton.h
@@ -1,47 +1,51 @@
 #ifndef WEB_SERVER_SINGLETON
 #define WEB_SERVER_SINGLETON
 
+#include <WebServer/base/CurrentThread.h>
+
 #include <boost/noncopyable.hpp>
+
 #include <pthread.h>
 #include <stdlib.h> //::atexit
 
 namespace ywl
 {
-
 template<typename T>
 class Singleton : public boost::noncopyable
 {
 public:
     static T& instance()
     {
-        pthread_once(&ponce_, &Singleton::init);
+__asm__ __volatile__("":::"memory");
+        if (value_ == NULL) {
+            pthread_mutex_lock(&mutex_);
+            if (value_ == NULL) {
+                value_ = new T();
+                ::atexit(Singleton::destroy);
+            }
+__asm__ __volatile__("":::"memory");
+            pthread_mutex_unlock(&mutex_);
+        }
         return *value_;
     }
 private:
     Singleton() = delete;
     ~Singleton() = delete;
-    static void init()
-    {
-        value_ = new T();
-        ::atexit(Singleton::destroy);
-    }
     static void destroy()
     {
-        typedef char T_MUST_BE_COMPLETE_TYPE[sizeof(T) == 0 ? -1 : 1];
-        T_MUST_BE_COMPLETE_TYPE check; (void)check;
         delete value_;
     }
 
 private:
     static T* value_;
-    static pthread_once_t ponce_;
+    static pthread_mutex_t mutex_;
 };
 
 template<typename T>
 T* Singleton<T>::value_ = NULL;
 
 template<typename T>
-pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+pthread_mutex_t Singleton<T>::mutex_ = PTHREAD_MUTEX_INITIALIZER;
 
 }//ywl
 #endif
